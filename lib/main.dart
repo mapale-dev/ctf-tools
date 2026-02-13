8feecf2ac267640ab30da224587b7fae5227a2da
MapleLeaf <197728340@qq.com>
2026-02-13 22:38:50 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: getRoute,
+      theme: ThemeData(
+        fontFamily: "MapleFont"
+      ),
     );
   }
 }
